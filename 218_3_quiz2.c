487708f3ba8f60024c7ae97d45e4c3e64b1f4c4f
6320501553 <78528282+6320501553@users.noreply.github.com>
2021-03-25 19:02:28 +0700

    edit

--- 218_3_quiz2.c
+++ 218_3_quiz2.c
@@ -2,8 +2,55 @@
 int main()
 {
     int n,i,j,k,sum=0,s,u,f,d,a,b,c,e,g,h;
+    int m[1000000];
     scanf("%d",&n);
-    if(n<10)
+    for(i=1;i<10;i++)
+    {
+        m[i]=i;
+    }
+    for(i=10;i<100;i++)
+    {
+        for(k=1; k<10; k++)
+        {
+            for(j=0; j<10; j++)
+            {
+                if(k==j)
+                    m[i]=(k*10)+j;
+            }
+        }
+    }
+    for(i=100;i<1000;i++)
+    {
+        for(k=1; k<10; k++)
+        {
+            for(j=0; j<10; j++)
+            {
+                for(i=0; i<10; i++)
+                {
+                    if(k==i)
+                        m[i]=(k*100)+(j*10)+i;
+                }
+            }
+        }
+    }
+    for(i=1000;i<10000;i++)
+    {
+        for(k=1; k<10; k++)
+        {
+            for(j=0; j<10; j++)
+            {
+                for(i=0; i<10; i++)
+                {
+                    for(u=0; u<10; u++)
+                    {
+                        if(k==u&&i==j)
+                            m[i]=(k*1000)+(j*100)+(i*10)+u;
+                    }
+                }
+            }
+        }
+    }
+    /*if(n<10)
         printf("%d",n);
     else if(n>9&&n<=100)
     {
@@ -14,13 +61,10 @@ int main()
             s=n/10;
         for(k=1; k<s; k++)
         {
-            for(i=0; i<=a; i++)
+            for(j=i; j<10; j++)
             {
-                for(j=0; j<10; j++)
-                {
-                    if(k==j)
-                        sum++;
-                }
+                if(k==j)
+                    sum++;
             }
         }
         printf("%d",sum+9);
@@ -110,6 +154,6 @@ int main()
             }
         }
         printf("%d",sum+1098);
-    }
+    }*/
     return 0;
 }
